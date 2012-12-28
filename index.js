7bca79d37b244210c04d55a5bee765f13e3398ca
Enrico Marino <enrico.marino@email.com>
2012-12-28 14:52:30 +0100

    added List#any

--- index.js
+++ index.js
@@ -425,3 +425,37 @@ List.prototype.none = function(fn){
 
   return true;
 };
+
+/*
+ * any
+ * Assert that at least one invocation of `fn(val, i)` is truthy.
+ *
+ * For example checking to see if any pets are ferrets:
+ *
+ *    pets.any(function(pet){
+ *      return pet.species == 'ferret'
+ *    })
+ *
+ * @param {Function} fn
+ * @return {Boolean}
+ * @api public
+ */
+
+List.prototype.any = function(fn){
+  var items = this._items;
+  var len = items.length;
+  var i;
+  var item;
+  var test;
+  var result = [];
+
+  for (i = 0; i < len; i += 1) {
+    item = items[i];
+    test = fn(item, i);
+    if (test) {
+      return true;
+    }
+  }
+
+  return false;
+};
