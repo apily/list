af5b237e8c9b8b0bac0aee545e3ce31b10f4ba6e
Enrico Marino <enrico.marino@email.com>
2012-12-28 14:46:22 +0100

    added List#none

--- index.js
+++ index.js
@@ -393,3 +393,35 @@ List.prototype.every = function(fn){
 
   return true;
 };
+
+/*
+ * Assert that none of the invocations of `fn(val, i)` are truthy.
+ *
+ * For example ensuring that no pets are admins:
+ *
+ *    pets.none(function(p){ return p.admin })
+ *    pets.none('admin')
+ *
+ * @param {Function|String} fn
+ * @return {Boolean}
+ * @api public
+ */
+
+List.prototype.none = function(fn){
+  var items = this._items;
+  var len = items.length;
+  var i;
+  var item;
+  var test;
+  var result = [];
+
+  for (i = 0; i < len; i += 1) {
+    item = items[i];
+    test = fn(item, i);
+    if (test) {
+      return false;
+    }
+  }
+
+  return true;
+};
