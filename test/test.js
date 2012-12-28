2f20dedbabeb831a4b6699ecc37237e7dc9e659f
Enrico Marino <enrico.marino@email.com>
2012-12-28 13:10:09 +0100

    test List#add

--- test/test.js
+++ test/test.js
@@ -1,6 +1,24 @@
 var List = require('list');
 var assert = require('component-assert');
 
+describe('List#add(item)', function(){
+  it('should emit `add` event', function(done){
+    var list = List([]);
+    var value = 'hello';
+
+    assert(list._items.length === 0);
+
+    list.on('add', function (item) {
+      assert(list._items.length === 1);
+      assert(list._items[0] === item);
+      assert(value === item);
+      done();
+    });
+
+    list.add(value);
+  })
+})
+
 describe('List#each(fn)', function(){
   it('should iterate each value', function(){
     var list = List([1,2,3]);
